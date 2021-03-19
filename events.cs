dc913fd423840c3a25048c4b2aecd53cab0fdcd8
ravuri-malleswari <71822755+ravuri-malleswari@users.noreply.github.com>
2021-03-19 16:42:42 +0530

    Add files via upload

---
+++ events.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        ProcessBusinessLogic bl = new ProcessBusinessLogic();
+        bl.ProcessCompleted += bl_ProcessCompleted; // register with an event
+        bl.StartProcess();
+    }
+
+    // event handler
+    public static void bl_ProcessCompleted()
+    {
+        Console.WriteLine("Process Completed!");
+    }
+}
+
+public delegate void Notify();  // delegate
+
+public class ProcessBusinessLogic
+{
+    public event Notify ProcessCompleted; // event
+
+    public void StartProcess()
+    {
+        Console.WriteLine("Process Started!");
+        // some code here..
+        OnProcessCompleted();
+    }
+
+
+    protected virtual void OnProcessCompleted()
+    {
+        ProcessCompleted?.Invoke();
+    }
+}
